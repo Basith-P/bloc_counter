371ca663b5152db6414e0aa2263c68f45ba502a1
Basith P <basithpofficial@gmail.com>
2023-03-24 00:19:12 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/bns/bloc_counter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bns.bloc_counter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/counter_observer.dart
@@ -0,0 +1,12 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart' show debugPrint;
+
+class CounterObserver extends BlocObserver {
+  const CounterObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    debugPrint('${bloc.runtimeType} $change');
+  }
+}
